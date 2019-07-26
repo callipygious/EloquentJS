100284471387e5d95d8ca4c1d0b98e5050b32a51
Callipygious <52933193+callipygious@users.noreply.github.com>
2019-07-26 13:43:11 -0400

    second problem done. Couldnt figure it, cheated,

--- Chapter4/4.2.js
+++ Chapter4/4.2.js
@@ -12,9 +12,19 @@ let reverseArray = function (arr) {
   return newArr;
 }
 
-
+/*
 let reverseArrayInPlace = function (arr) {
   for (let n = 0; n <= length.arr; n++) {
     let wal = arr.pop();
     arr.unshift(wal);
 }}
+ THis was trash and doesnt work. Cheated and read answer for this. */
+
+ function reverseArrayInPlace (arr) { //just copying the answer from website, adding comments to help understand
+   for (let i = 0; i < Math.floor(arr.length / 2); i++) { //Loop iterates thru the first half of the array (length/2 is half the elements, math floor rounds down for odd #s since middle element doesnt move anyway.)
+     let old = arr[i]; //this acts as a register for saving the lefthand element in the array  (starting with first(0th) element (i))
+     arr [i] = arr [arr.length - 1 - i]; //Replaces the ith element in the array with its "opposite" from the second half of the array. i.e. first element becomes the last, 2nd becomes 2nd last, etc
+     arr [arr.length - 1 - i] = old; //Replaces the 2nd-half-mirror value with the saved original 1st-half value, completing a successful flip for the corresponding elements in this iteration of the loop. length-1 takes into account the element count starting at 0, then subtracting i moves to the specific element that is the flipped partner for the ith element in the first half.
+ }
+ return arr;
+ }
