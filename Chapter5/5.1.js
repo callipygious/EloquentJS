6dd6bf760707de59fd896237d19c8ccbffdd3020
Callipygious <52933193+callipygious@users.noreply.github.com>
2019-08-01 21:23:37 -0400

    more chapter 5. Gotta figure out how to deal with emacs autosaves.

--- Chapter5/5.1.js
+++ Chapter5/5.1.js
@@ -2,9 +2,22 @@
 
 Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays. */
 
+let arrays = [[1, 2, 3], [4, 5], [6]];
 let flatten = function (array) {
   let flat = [];
   for (ele of array) {
-    
-  }
-}
+      flat.concat(array.reduce((x) => {
+	  for (ele of x) {
+	      return ele;
+	  }}
+	  ,0)
+		 ) }
+    return flat ;}
+
+console.log(flatten(arrays));
+
+//^ "x is not iterable" doesnt work at all. Cheating, answer from site:
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(arrays.reduce((flat, current) => flat.concat(current), [])); //Starts with a new empty array, concats the "current" value onto it as it moves thru the array using reduce on each item.
