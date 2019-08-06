8b16104e3b7dacf697f7b4da9bac0f16ede248f3
Callipygious <52933193+callipygious@users.noreply.github.com>
2019-08-06 19:57:46 -0400

    First half of ch 6. Got rid of emacs save artifacts since im gonna use atom w/ this book from here on out.

--- Chapter6/6.1.js
+++ Chapter6/6.1.js
@@ -6,4 +6,42 @@ Give the Vec prototype two methods, plus and minus, that take another vector as 
 
 Add a getter property length to the prototype that computes the length of the vector-that is, the distance of the point (x, y) from the origin (0, 0). */
 
+class Vec {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+    }
+    plus(vec2) {
+      let newX = this.x+vec2.x;
+      let newY = this.y+vec2.y;
+      return new vec (newX, newY);
+    }
+    minus(vec2) {
+      let newX = this.x-vec2.x;
+      let newY = this.y-vec2.y;
+      return new vec (newX, newY);
+    }
+    get length() {
+      return  `This vector is ${Math.sqrt((this.x*this.x)+(this.y*this.y))} long!`  //Just wanted to try the template literal format here. Not using "get" operator just made the program return the entire function rather than computing anything. Weird.
+    }
+}
 
+// Official Version:
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(other) {
+    return new Vec(this.x + other.x, this.y + other.y);
+  }
+
+  minus(other) {
+    return new Vec(this.x - other.x, this.y - other.y);
+  }
+
+  get length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+}
